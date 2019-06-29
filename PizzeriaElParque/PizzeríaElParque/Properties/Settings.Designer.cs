8dd4a901d0d4e7b8bd89b79a8c3742e3065ccf3d
richard0492 <45183927+richard0492@users.noreply.github.com>
2019-06-28 18:44:57 -0600

    Cambos

--- PizzeriaElParque/PizzeríaElParque/Properties/Settings.Designer.cs
+++ PizzeriaElParque/PizzeríaElParque/Properties/Settings.Designer.cs
@@ -8,28 +8,36 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PizzeríaElParque.Properties {
-    
-    
+namespace PizzeríaElParque.Properties
+{
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
             }
         }
 
-        
+
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string Valor {
-            get {
+        public string Valor
+        {
+            get
+            {
                 return ((string)(this["Valor"]));
             }
         }
+    }
+}
 
